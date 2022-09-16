c46c15c76087e3f4c9d07a7941025dd8e50db705
Charlieheber <66089439+Charlieheber@users.noreply.github.com>
2022-09-16 18:36:51 +0100

    [Update] Begin calculation of fuel moisture for weather data at kettleman hills. Also a bit of visualisation.

--- scripts/fire simulation model/get_fuel_moisture_content.R
+++ scripts/fire simulation model/get_fuel_moisture_content.R
@@ -1,8 +1,105 @@
 rm(list=ls())
+library(tidyverse)
 library(data.table)
 
+# functions ########################
+####################################
 
-file_loc <- paste0(here::here(),  "/data/input/wildfire simulation model/")
+fns_loc <- paste0(here::here(), "/scripts/R/fire simulation model/fns/")
+source(paste0(fns_loc, "fuel_moisture_content_fns.R"))
+
+# load station data #################
+#####################################
+
+file_loc <- paste0(here::here(),  "/data/input/wildfire simulation model/station data/")
+this_station_data <- fread(paste0(file_loc, "station_data_kettleman_hills_00_22.csv"))
+
+# useful quantities ###############
+###################################
+
+mm_to_inches <- 0.0393701
+
+# get state of weather code ########
+####################################
+
+state_of_weather_key <- data.table(
+  "state_of_weather" = c(0:6),
+  "weather" = c("clear", "scattered clouds", "broken clouds", "overcast", "fog", "drizzle", "rain")
+)
+
+head(this_station_data)
+
+# need to find yearly maximum solar radiation
+get_year_max_solar_radiation <- function(station_data){
+  
+  station_data_lst_by_year <- split(station_data, station_data$year)
+  
+  max_solar_rad_lst <- lapply(station_data_lst_by_year, function(x) data.frame("max_year_solar_radiation" = max(x$total_solar_radiation)))
+    
+  max_solar_rad_df <- data.table(
+    "year" = as.numeric(names(max_solar_rad_lst)),
+    rbindlist(max_solar_rad_lst)
+  )
+  
+  return(max_solar_rad_df)
+  
+}
+
+this_year_max_solar_df <- get_year_max_solar_radiation(this_station_data)
+this_station_data <- this_station_data %>% left_join(this_year_max_solar_df, by="year")
+
+# get state of weather codes
+this_station_data$state_of_weather <- apply(this_station_data[,c("total_solar_radiation", "max_year_solar_radiation", "total_precipitation")],
+                                            1,
+                                            function(x) get_state_of_weather_code(x[1], x[2], x[3]*mm_to_inches)
+                                            )
+# check result
+state_of_weather_count <- this_station_data %>% count(state_of_weather)
+state_of_weather_count <- state_of_weather_count %>% left_join(state_of_weather_key, by="state_of_weather")
+
+ggplot(state_of_weather_count) + geom_bar(aes(x = weather, y = n), stat="identity")
+
+# get 1 hour dead fuel moisture content ###
+###########################################
+
+get_MC_1hr(this_station_data$ave_relative_humidity[1]/100, celsius_to_fahrenheit(this_station_data$ave_air_temp[1]),
+           this_station_data$state_of_weather[1], FALSE, FALSE)
+
+this_station_data$MC_1hr <- apply(this_station_data[, c("ave_relative_humidity", "ave_air_temp", "state_of_weather")], 1,
+                                  function(x) get_MC_1hr(x[1]/100, celsius_to_fahrenheit(x[2]), x[3], FALSE))
 
 
-fread(paste0(file_loc, "station_data_ash_meadows_nevada_sept_2016.csv"))
+# get 10 hour dead fuel moisture content ##
+###########################################
+
+get_MC_10hr(this_station_data$ave_relative_humidity[1]/100, celsius_to_fahrenheit(this_station_data$ave_air_temp[1]))
+
+this_station_data$MC_10hr <- apply(this_station_data[, c("ave_relative_humidity", "ave_air_temp")], 1,
+                                  function(x) get_MC_10hr(x[1]/100, celsius_to_fahrenheit(x[2])))
+
+
+# get 100 hour dead fuel moisture content #
+###########################################
+
+this_station_data$daylight_hours <- get_daylight_hours(this_station_data$latitude, this_station_data$day_of_year)
+
+this_station_data$MC_10hr <- apply(this_station_data[, c("ave_relative_humidity", "ave_air_temp")], 1,
+                                   function(x) get_MC_100hr(x[1]/100, celsius_to_fahrenheit(x[2])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/fire simulation model/visualise_fuel_moisture_vars.R
@@ -0,0 +1,45 @@
+rm(list=ls())
+library(data.table)
+library(ggplot2)
+
+# functions ########################
+####################################
+
+fns_loc <- paste0(here::here(), "/scripts/R/fire simulation model/fns/")
+source(paste0(fns_loc, "fuel_moisture_content_fns.R"))
+
+# equilbrium moisture content #####
+###################################
+
+# dummy data
+# constant temp
+df1_const_temp <- data.frame(
+  "RH" = rep(c(1:100)/100,4),
+  "TEMP_c" = c(rep(0,100), rep(10,100), rep(20,100), rep(30,100)),
+  "constant temperature" = c(rep("0 degrees",100), rep("10 degrees",100), rep("20 degrees",100), rep("30 degrees",100))
+)
+
+# constant RH
+df2_const_RH <- data.frame(
+  "RH" = c(rep(0.05, 35), rep(0.20, 35), rep(0.50, 35), rep(0.80, 35)),
+  "TEMP_c" = 1:35,
+  "relative humidity" = c(rep("5%",35), rep("20%",35), rep("50%",35), rep("80%",35))
+)
+
+# to fahrenheit
+df1_const_temp$TEMP_f <- celsius_to_fahrenheit(df1_const_temp$TEMP_c)
+df2_const_RH$TEMP_f <- celsius_to_fahrenheit(df2_const_RH$TEMP_c)
+
+# get EMC
+df1_const_temp$EMC <- apply(df1_const_temp[, c("RH", "TEMP_f")], 1, function(x) get_EMC(x[1], x[2]))
+df2_const_RH$EMC <- apply(df2_const_RH[, c("RH", "TEMP_f")], 1, function(x) get_EMC(x[1], x[2]))
+
+# plot
+ggplot(data=df1_const_temp, aes(x=RH, y=EMC, group=constant.temperature)) +
+  geom_line()
+
+ggplot(data=df2_const_RH, aes(x=TEMP_c, y=EMC, group=relative.humidity)) +
+  geom_line()
+
+
+
